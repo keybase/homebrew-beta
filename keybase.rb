81752cef0a6ccf74bdb168bad7fece3a1b675923
Gabriel Handford <gabrielh@gmail.com>
2015-09-11 16:55:49 -0700

    Fixing

--- keybase.rb
+++ keybase.rb
@@ -3,16 +3,16 @@ class Keybase < Formula
   homepage "https://keybase.io/"
 
   url "https://github.com/keybase/client-beta/archive/v1.0.0-18.tar.gz"
-  sha256 "fa6d91ca154de050eebeb6ecf25c6d083720c86e4f6977084a760b56d7fe4f20"
+  sha256 "eb8d875b156f23e0e48ce990bbc4e93c3de66beefc959f71a191b05de982f98f"
 
   head "https://github.com/keybase/client-beta.git"
   version "1.0.0-18"
 
-  bottle do
-    cellar :any_skip_relocation
-    root_url "https://github.com/keybase/client-beta/releases/download/v1.0.0-18/"
-    sha256 "44e2e10b93aa4ec5d17ed6d10ca50b2ac518c0f2ac26d03fdbc0247eed3fb753" => :yosemite
-  end
+  # bottle do
+  #   cellar :any_skip_relocation
+  #   root_url "https://github.com/keybase/client-beta/releases/download/v1.0.0-18/"
+  #   sha256 "44e2e10b93aa4ec5d17ed6d10ca50b2ac518c0f2ac26d03fdbc0247eed3fb753" => :yosemite
+  # end
 
   depends_on "go" => :build
 
