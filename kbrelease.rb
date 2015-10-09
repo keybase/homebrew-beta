2d3e81b8042253a4e64a3d79d44be8ee4983b098
Patrick Crosby <pcrosby@gmail.com>
2015-10-09 10:13:24 -0500

    Initial version of kbrelease.rb

---
+++ kbrelease.rb
@@ -0,0 +1,38 @@
+class Kbprod < Formula
+  desc "Keybase (Production)"
+  homepage "https://keybase.io/"
+
+  url "https://github.com/keybase/client-beta/archive/v1.0.0-30.tar.gz"
+  sha256 "8ded13a9650d9b80f5afe5a52cab33ae94515b5317d9a72c88ca7f45e9a5e005"
+
+  head "https://github.com/keybase/client-beta.git"
+  version "1.0.0-30"
+
+  depends_on "go" => :build
+
+  # bottle do
+  #   cellar :any_skip_relocation
+  #   sha256 "c04b868ba22b8ce2742d3031c9b342cdaa4e5629915e9219e1fa3cb5938c2d07" => :yosemite
+  #   root_url "https://github.com/keybase/client-beta/releases/download/v1.0.0-27"
+  # end
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GOBIN"] = buildpath
+    ENV["GO15VENDOREXPERIMENT"] = "1"
+    system "mkdir", "-p", "src/github.com/keybase/"
+    system "mv", "client", "src/github.com/keybase/"
+
+    system "go", "build", "-a", "-tags", "release brew", "-o", "kbprod", "github.com/keybase/client/go/keybase"
+
+    bin.install "kbprod"
+  end
+
+  def post_install
+    system "#{opt_bin}/kbprod", "launchd", "install", "homebrew.mxcl.keybase.production", "#{opt_bin}/kbprod", "service"
+  end
+
+  test do
+    system "#{bin}/kbprod", "version"
+  end
+end
