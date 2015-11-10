a296f00653f8866388e7f4e1c653492dbf5871ec
Jack O'Connor <oconnor663@gmail.com>
2015-11-10 17:52:05 -0500

    New kbstage version v1.0.0-46

--- kbstage.rb
+++ kbstage.rb
@@ -2,19 +2,19 @@ class Kbstage < Formula
   desc "Keybase (Staging)"
   homepage "https://keybase.io/"
 
-  url "https://github.com/keybase/client/archive/v1.0.0-45.tar.gz"
-  sha256 "51385ea2823affb8f4a94d462ad96052ebe4b20c05518276096236d45a3c9653"
+  url "https://github.com/keybase/client/archive/v1.0.0-46.tar.gz"
+  sha256 "383625f3b3d4c2c78e68defc1d90c5e611441f5da03f3eebc0c4c1ffc6d305cd"
 
   head "https://github.com/keybase/client.git"
-  version "1.0.0-45"
+  version "1.0.0-46"
 
   depends_on "go" => :build
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "bc9432e476c402b0be1c4ba92a657d31039b46062f0a9f2a837d8fbaf95bb063" => :yosemite
-    root_url "https://github.com/keybase/client/releases/download/v1.0.0-45"
-  end
+  # bottle do
+  #   cellar :any_skip_relocation
+  #   sha256 "c04b868ba22b8ce2742d3031c9b342cdaa4e5629915e9219e1fa3cb5938c2d07" => :yosemite
+  #   root_url "https://github.com/keybase/client/releases/download/v1.0.0-27"
+  # end
 
   def install
     ENV["GOPATH"] = buildpath
